aed553245c24f30ec3aa8864372506dbf67e5111
Mizuko Aoyagi <mizuko.11gako.18@gmail.com>
2018-10-10 00:28:54 +0900

    3 custom files have made on Oct 06, 2018
    DiaryTableViewCell.swift
    DiaryNode.swift
    Diary.swift

---
+++ MyBeerDiary2/Diary.swift
@@ -0,0 +1,13 @@
+//
+//  Diary.swift
+//  MyBeerDiary2
+//
+//  Created by 青柳瑞子 on 2018/10/06.
+//  Copyright © 2018 青柳瑞子. All rights reserved.
+//
+
+import Foundation
+
+struct Diary {
+    var nodes: [DiaryNode]?
+}
---
+++ MyBeerDiary2/DiaryNode.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryNode.swift
+//  MyBeerDiary2
+//
+//  Created by 青柳瑞子 on 2018/10/06.
+//  Copyright © 2018 青柳瑞子. All rights reserved.
+//
+
+import Foundation
+
+struct DiaryNode {
+    let date: String
+    let drink: String
+    let place: String?
+    let image: Data
+}
