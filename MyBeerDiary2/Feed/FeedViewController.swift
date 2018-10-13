ab8f0dd7c36cec334f2edf40bcd08236b71d8c5a
Florian Ludot <florian.ludot@gmail.com>
2018-10-13 15:28:29 +0900

    feedviewcontroller init

---
+++ MyBeerDiary2/Feed/FeedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FeedViewController.swift
+//  MyBeerDiary2
+//
+//  Created by Florian LUDOT on 10/13/18.
+//  Copyright © 2018 青柳瑞子. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        title = "Feed"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
