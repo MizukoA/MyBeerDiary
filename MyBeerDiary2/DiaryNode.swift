3bd8f5433407760c6115bff2cb3ce3e1ccc345b7
Florian Ludot <florian.ludot@gmail.com>
2018-10-13 18:41:47 +0900

    model update UIImage

--- MyBeerDiary2/DiaryNode.swift
+++ MyBeerDiary2/DiaryNode.swift
@@ -6,11 +6,11 @@
 //  Copyright © 2018 青柳瑞子. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct DiaryNode {
     let date: String
     let drink: String
     let place: String?
-    let image: Data
+    let image: UIImage
 }
